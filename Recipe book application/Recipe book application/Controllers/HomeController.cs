9a14dbf0f341cecf5f9d31d4b41f352d1c66eac6
Ville Terasahde <villeterasahde@gmail.com>
2021-02-18 21:41:06 +0200

    Project files created. Homecontroller and view added. Startup file path pattern set up.

---
+++ Recipe book application/Recipe book application/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Recipe_book_application.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index() // Login page
+        {
+            return View();
+        }
+    }
+}
