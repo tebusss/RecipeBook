87d97cbc1be88ca29acdbf5a4c348d66a5f5dd95
Ville Terasahde <villeterasahde@gmail.com>
2021-03-03 21:35:18 +0200

    EF added. RecipeContext and IdentityDataContext added

---
+++ Recipe book application/Recipe book application/DAL/IdentityDataContext.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Recipe_book_application.DAL
+{
+    public class IdentityDataContext
+    {
+    }
+}
---
+++ Recipe book application/Recipe book application/DAL/RecipeContext.cs
@@ -0,0 +1,23 @@
+﻿using System.Data.Entity;
+
+
+
+namespace Recipe_book_application.DAL
+{
+    public class RecipeContext : DbContext
+    {
+        public RecipeContext() : base("RecipeContext")
+        {
+                
+        }
+        //
+        //DbSets
+        //
+        //https://docs.microsoft.com/en-us/aspnet/mvc/overview/getting-started/getting-started-with-ef-using-mvc/creating-an-entity-framework-data-model-for-an-asp-net-mvc-application
+
+        protected override void OnModelCreating(DbModelBuilder dbModelBuilder)
+        {
+
+        }
+    }
+}
